aa6bdfb94548e1bc2f4c2a8ea054bfd0fb4a92d2
alunoctg <alunoctg@fiern.local>
2026-09-15 11:04:14 -0300

    passo 1 e passo 2 do token

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -3,16 +3,28 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
-  const [logado, setLogado] = useState(false);
+ 
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [usuario, setUsuario] = useState(null);
 
-  function login() {
-    setLogado(true);
+  function login(dadosUsuario, tokenRecebido) {
+    setUsuario(dadosUsuario);
+
+    setToken(tokenRecebido);
+
+    localStorage.setItem("token", tokenRecebido);
   }
+
   function logout() {
-    setLogado(false);
+    setUsuario(null);
+
+    setToken(null);
+
+    localStorage.removeItem("token");
   }
+
   return (
-    <AuthContext.Provider value={{ logado, login, logout }}>
+    <AuthContext.Provider value={{ token, usuario, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
@@ -22,7 +34,8 @@ export function useAuth() {
   const context = useContext(AuthContext);
 
   if (!context) {
-    throw new Error("useAuth deve ser usado dentro do AuthProvider");
+    throw new Error("useAuth fora do AuthProvider");
   }
-  return context; 
+
+  return context;
 }
