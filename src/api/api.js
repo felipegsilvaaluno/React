445b91cb256ddf14bd6cff7fec557ffd23f66854
alunoctg <alunoctg@fiern.local>
2026-09-18 10:10:55 -0300

    ajuste api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_TESTE,
+  baseURL: import.meta.env.VITE_API,
 });
 
 api.interceptors.request.use((config) => {
